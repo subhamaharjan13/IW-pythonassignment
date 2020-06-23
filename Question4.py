bf936d56c75684ebbf89d4b63c3890dcf06941bd
subhamaharjan <subhamaharjan13@gmail.com>
2020-06-23 17:49:16 +0545

    Question4 solved

---
+++ Question4.py
@@ -0,0 +1,10 @@
+# Write a python program to get a single string from two given strings, separated by a space and swap the first two characters of each string.
+
+first_string = 'abc'
+second_string = 'xyz'
+
+char1 = first_string.replace(first_string[0:2],second_string[0:2])
+char2 = second_string.replace(second_string[0:2],first_string[0:2])
+
+final_string = char1 + " " + char2
+print(final_string)
