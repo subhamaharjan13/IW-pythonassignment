e94086342e609a7fdc5b498f1a6cd95c06fc1898
subhamaharjan <subhamaharjan13@gmail.com>
2020-06-25 20:00:15 +0545

    question 30 solved

---
+++ Q30.py
@@ -0,0 +1,9 @@
+# Write a Python script to check whether a given key already exists in a dictionary.
+
+
+dictionary_data= {"key1": 5, "key2": 10}
+
+if "key1" in dictionary_data:
+    print("Key1 exists in the dictionary")
+else:
+    print("Key doesn't exists in the dictionary")
