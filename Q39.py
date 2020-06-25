c451fc5789a418edd5fbc4421df4f1be009c2fa2
subhamaharjan <subhamaharjan13@gmail.com>
2020-06-25 21:27:24 +0545

    question 39 solved

---
+++ Q39.py
@@ -0,0 +1,9 @@
+# Write a Python program to unpack a tuple in several variables.
+
+tuple_data = ('kathmandu','delhi', 'california')
+
+(Nepal,India,America) = tuple_data
+
+print(Nepal)
+print(India)
+print(America)
