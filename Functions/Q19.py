b7241e91736cf081d5f1d2e6dbe5bd09eae14e4a
subhamaharjan <subhamaharjan13@gmail.com>
2020-06-26 17:15:17 +0545

    fibo series using lambda

---
+++ Functions/Q19.py
@@ -0,0 +1,7 @@
+# Write a Python program to create Fibonacci series upto n using Lambda.
+
+from functools import reduce
+
+fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+
+print(fib(10))
