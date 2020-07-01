6ad477d3a83fcce4df24d9029540411fe6c0fc68
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-01 20:09:54 +0545

    removed unused result variable

--- Functions/Q20.py
+++ Functions/Q20.py
@@ -3,5 +3,4 @@
 array1 = [2,4,6,8,0,11,13,16]
 array2 = [1,3,7,9,11,13,15]
 
-intersection = list(filter(lambda x: x in array1, array2))
-print(intersection)
+print("intersection:", list(filter(lambda x: x in array1, array2)))
