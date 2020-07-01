7a7bdc25fc62e5b891b591d7f90373e8fd751b58
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-01 19:49:38 +0545

    replaced unused resultvariable

--- Datatypes/Q29.py
+++ Datatypes/Q29.py
@@ -4,6 +4,4 @@ dict1={1:10, 2:20}
 dict2={3:30, 4:40}
 dict3={5:50,6:60}
 
-result_dict = {**dict1, **dict2, **dict3}
-
-print(result_dict)
+print("Concatenated dictionary is ",{**dict1, **dict2, **dict3})
