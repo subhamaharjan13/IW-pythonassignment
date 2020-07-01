81b0a7e14941503286609ec78b0aa99cff1dfa61
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-01 20:04:00 +0545

    replaced unused result variable

--- Datatypes/Question8.py
+++ Datatypes/Question8.py
@@ -4,9 +4,4 @@ string = "character"
 
 n = 1
 
-first_part = string[:n]
-last_part = string[n+1:]
-
-removed_string = first_part + last_part
-
-print(removed_string)
+print("removed the nth index character from string:", string[:n] + string[n+1:])
