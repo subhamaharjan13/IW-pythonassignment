82387a85d7a4f7eb0324bd2c8948c4e221ea1964
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-01 20:24:23 +0545

    leap year

---
+++ PythonAssignment-II/Q02.py
@@ -0,0 +1,14 @@
+# Write an if statement to determine whether a variable holding a year is a leap year.
+
+year = 2020
+
+if (year % 4) == 0:
+    if (year % 100 ) == 0:
+        if (year % 400) == 0:
+            print("{0} is a leap year".format(year))
+        else:
+            print("{0} is not a leap year".format(year))
+    else:
+        print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
