290fed58d14144164fa45e22d4b5686686123bd4
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-03 16:46:14 +0545

    exception handling of division by zero

---
+++ PythonAssignment-II/Q17.py
@@ -0,0 +1,20 @@
+# Write a program that serves as a basic calculator. It asks for two numbers, then it asks for an operator.
+# Gracefully deal with input that doesn't cleanly convert to numbers. Deal with division by zero errors.
+
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter another number:"))
+op = input("Enter the operator:")
+
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == '/':
+    try:
+        print(num1 / num2)
+    except Exception as e:
+        print(e)
+else:
+    print("Invalid Operator")
