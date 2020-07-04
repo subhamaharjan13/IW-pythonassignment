cec32ff9f2dc7afaa58d7e332501702821a2491d
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-04 09:33:04 +0545

    adds comment

--- PythonAssignment-II/Q19.py
+++ PythonAssignment-II/Q19.py
@@ -2,15 +2,16 @@
 # These brackets must be close in the correct order, for example "()" and "()[]{}" are valid but "[)", "({[)]" and "{{{" are invalid
 
 class Parentheses:
-   def is_valid(self, string):
-        stack = []
-        brackets = {"(": ")", "{": "}", "[": "]"}
-        for symbol in string:
-            if symbol in brackets:
-                stack.append(symbol)
-            elif len(stack) == 0 or brackets[stack.pop()] != symbol:
-                return False
-        return len(stack) == 0
+    # check if the parenthesis is valid or not
+    def is_valid(self, string):
+            stack = []
+            brackets = {"(": ")", "{": "}", "[": "]"}
+            for symbol in string:
+                if symbol in brackets:
+                    stack.append(symbol)
+                elif len(stack) == 0 or brackets[stack.pop()] != symbol:
+                    return False
+            return len(stack) == 0
 
 
 print(Parentheses().is_valid("(){}[]"))
