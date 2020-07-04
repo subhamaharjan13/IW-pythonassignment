aa2d9e23a0c0b72fc25e155470864ad18be847c3
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-04 08:49:16 +0545

    adds comment

--- PythonAssignment-II/Q01.py
+++ PythonAssignment-II/Q01.py
@@ -1,6 +1,7 @@
 # Create a variable, paragraph, that has the following content:
 # "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
 
+# creating a string variable that contains a paragraph of text
 paragraph = """ "Python is a great language!", said Fred. "I don't ever remember having this much fun before." """
 
 print(paragraph)
