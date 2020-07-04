1753d82dff69e9c95de99212066eb7b38a9b06e3
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-04 08:55:10 +0545

    adds comment and rename a variable

--- PythonAssignment-II/Q06.py
+++ PythonAssignment-II/Q06.py
@@ -1,11 +1,12 @@
 # Create a list with the names of friends and colleagues. Search for the
 # name ‘John’ using a for a loop. Print ‘not found’ if you didn't find it.
 
-nameList = ["Joey","Chandler","Paul","Monica","Ross","Phoebe","Rachel"]
+nameList = ["Joey","Chandler","Paul","Monica","Ross","Phoebe","Rachel","John"]
 
-for x in range(len(nameList)):
-    if "john" in nameList[x].lower():
+# search for name "John" in the list nameList
+for name in range(len(nameList)):
+    if "john" in nameList[name].lower():
         print("John exists")
         break
-    elif "john" not in nameList[x].lower() and x == len(nameList) - 1:
+    elif "john" not in nameList[name].lower() and name == len(nameList) - 1:
         print("Not found")
